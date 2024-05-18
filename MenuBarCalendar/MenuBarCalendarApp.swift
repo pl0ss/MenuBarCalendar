488152eb200d092686609babc2d187fba34d9689
Kevin Ploß <65730791+pl0ss@users.noreply.github.com>
2024-05-18 10:36:04 +0200

    BugFix

--- MenuBarCalendar/MenuBarCalendarApp.swift
+++ MenuBarCalendar/MenuBarCalendarApp.swift
@@ -19,16 +19,18 @@ class Event {
     var startDate: Date
     var endDate: Date
     var location: String
-    var differenct_date: Bool // für die Unterteilung zischen Heute und Morgen
+    var differenct_date: Bool // für die Unterteilung zischen verschiedenen Tagen
     var most_important_event: Bool  // Event, welches in der MenuBar angezigt wird, hervorheben
+    var multiple_days_info: String // wenn ein event über mehrere tage geht, dann von bis anzeigen
     
-    init(title: String, startDate: Date, endDate: Date, location: String, differenct_date: Bool, most_important_event: Bool) {
+    init(title: String, startDate: Date, endDate: Date, location: String, differenct_date: Bool, most_important_event: Bool, multiple_days_info: String) {
         self.title = title
         self.startDate = startDate
         self.endDate = endDate
         self.location = location
         self.differenct_date = differenct_date
         self.most_important_event = most_important_event
+        self.multiple_days_info = multiple_days_info
     }
 }
 
@@ -77,19 +79,28 @@ struct AppMenu: View {
         // ToDo Buttons zu Text
         
         return VStack {
-            Button(action: action1, label: { Text("Heute").font(.system(size: 14, weight: .bold)) })
             
             ForEach(myEvents.indices, id: \.self) { index in
-                // für die Unterteilung zischen Heute und Morgen
+                if(index == 0) {
+                    // meistens "Heute"
+                    Button(action: action1, label: { Text(date_to_datumName_long(date: myEvents[0].startDate)).font(.system(size: 14, weight: .bold)) })
+                }
+                
+                // für die Unterteilung zischen verschiedenen Tagen
                 if(myEvents[index].differenct_date) {
+                    // meistens "Morgen"
                     Divider()
-                    Button(action: action1, label: { Text("Morgen").font(.system(size: 14, weight: .bold)) })
+                    Button(action: action1, label: { Text(date_to_datumName_long(date: myEvents[index].startDate)).font(.system(size: 14, weight: .bold)) })
                 }
                 
                 if myEvents[index].most_important_event { // Event, welches in der MenuBar angezigt wird, hervorheben
                     Button(action: action1, label: { Text(getActionText(num: index)).underline() })
                 } else {
                     Button(action: action1, label: { Text(getActionText(num: index)) })
+                }
+                
+                if(myEvents[index].multiple_days_info != "") {
+                    Text(myEvents[index].multiple_days_info)
                 }
 
             }
@@ -113,30 +124,14 @@ struct AppMenu: View {
     }
 }
 
-func dateOnly(date: Date) -> String { // entfernt die uhrzeit
-    if date == nil {
-        return ""
-    }
-
-    let dateFormatter = DateFormatter()
-    dateFormatter.timeZone = TimeZone.current
-    dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
-    let dateLocal = dateFormatter.string(from: date)
-    
-    return dateLocal.components(separatedBy: " ")[0]
-}
-
-
 func getNextEvents() -> [Event] {
     let eventStore = EKEventStore()
     
     eventStore.requestFullAccessToEvents { (granted, error) in
         if granted {
             print("Zugriff auf Kalender genehmigt")
-            // Hier können Sie Ihre Logik ausführen, die auf den Zugriff zugreift
         } else {
             print("Zugriff auf Kalender abgelehnt oder Fehler aufgetreten: \(error?.localizedDescription ?? "Unbekannter Fehler")")
-            // Hier können Sie eine Fehlerbehandlung implementieren
         }
     }
     
@@ -165,16 +160,9 @@ func getNextEvents() -> [Event] {
         events = eventStore.events(matching: aPredicate)
     }
     
-
-    let nowUTC = getUTCnow()
-    // let utcDateString = dateFormatter.string(from: nowUTC)
-    // print("Aktuelle UTC-Zeit: \(utcDateString)")
-    
-    
     var nextEvents: [Event] = []
-
-    
     var lastEvent: Event? = nil
+    let now = date_getNow()
     var most_important_event_set = false
     
     // Events ausgeben
@@ -186,25 +174,32 @@ func getNextEvents() -> [Event] {
             // print("End Date: \(event.endDate)")
             // print("")
             
-            // if(event.startDate > nowUTC) {
-            if(event.endDate > nowUTC) {
+            if(date_to_local(date: event.endDate) > now) {
                 // return event.title
                 
-                var differenct_date = false // für die Unterteilung zischen Heute und Morgen
+                var differenct_date = false // für die Unterteilung zischen verschiedenen Tagen
                 if lastEvent != nil {
-                    if (lastEvent != nil) && dateOnly(date: lastEvent!.startDate) != dateOnly(date: event.startDate) {
+                    if (lastEvent != nil) && date_to_date_string(date: lastEvent!.startDate) != date_to_date_string(date: event.startDate) {
                         differenct_date = true
                     }
                 }
                 
                 var most_important_event = false // Event, welches in der MenuBar angezigt wird, hervorheben
-                let nowUTC = getUTCnow()
-                if event.startDate > nowUTC && !most_important_event_set {
+                let now = date_getNow()
+                if date_to_local(date: event.startDate) > now && !most_important_event_set {
                     most_important_event = true
                     most_important_event_set = true
                 }
                 
-                let thisEvent = Event(title: event.title, startDate: event.startDate, endDate: event.endDate, location: event.location ?? "", differenct_date:  differenct_date, most_important_event: most_important_event)
+                var multiple_days_info = ""
+                let dateStringStart = date_to_datumName(date: event.startDate)
+                let dateStringEnd = date_to_datumName(date: event.endDate)
+                if dateStringStart != dateStringEnd {
+                    multiple_days_info = " • \(dateStringStart) - \(dateStringEnd)"
+                }
+                
+                
+                let thisEvent = Event(title: event.title, startDate: event.startDate, endDate: event.endDate, location: event.location ?? "", differenct_date:  differenct_date, most_important_event: most_important_event, multiple_days_info: multiple_days_info)
                 
                 nextEvents.append(thisEvent)
                 lastEvent = thisEvent
@@ -225,9 +220,9 @@ func getMenuBarText() -> String {
     var nextEvent: Event? = nil
     var lastEvent: Event? = nil
     
-    let nowUTC = getUTCnow()
+    let now = date_getNow()
     for i in 0..<myEvents.count {
-        if myEvents[i].startDate > nowUTC {
+        if date_to_local(date: myEvents[i].startDate) > now {
             nextEvent = myEvents[i]
             
             if(i > 0) {
@@ -324,10 +319,175 @@ func eventToActionText(event: Event) -> String {
     return actionText
 }
 
-func getUTCnow() -> Date {
+
+//* Basics Date Functions
+// =======================================================================
+// =======================================================================
+
+func date_to_local(date: Date) -> Date {
+    let calendar = Calendar.current
+    let timeZone = TimeZone.current
+    let components = calendar.dateComponents(in: timeZone, from: date)
+    
+    return calendar.date(from: components)!
+}
+
+func date_getUTCnow() -> Date {
     let dateFormatter = DateFormatter()
-    dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
     dateFormatter.timeZone = TimeZone(identifier: "UTC")
+    dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
     let nowUTC = Date()
     return nowUTC
+}
+
+func date_getNow() -> Date {
+    let dateFormatter = DateFormatter()
+    dateFormatter.timeZone = TimeZone.current
+    dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+    let now = Date()
+    return now
+}
+
+func string_to_date(str: String) -> Date {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+    return dateFormatter.date(from: str)!
+}
+
+func date_offset(date: Date = date_getNow(), years: Int = 0, months: Int = 0, days: Int = 0, hours: Int = 0, minutes: Int = 0, seconds: Int = 0, mitternacht: Bool = false) -> Date {
+    var calendar = Calendar.current
+    var dateComponents = DateComponents()
+    
+    dateComponents.year = years
+    dateComponents.month = months
+    dateComponents.day = days
+    dateComponents.hour = hours
+    dateComponents.minute = minutes
+    dateComponents.second = seconds
+    
+    guard let newDate = calendar.date(byAdding: dateComponents, to: date) else {
+        return date
+    }
+    
+    if mitternacht {
+        return calendar.startOfDay(for: newDate)
+    }
+    
+    return newDate
+}
+
+func date_to_dateTime_string(date: Date) -> String { // "yyyy-MM-dd HH:mm:ss"
+    if date == nil {return ""}
+    
+    let dateFormatter = DateFormatter()
+    dateFormatter.timeZone = TimeZone.current
+    dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+    let dateStringLocal = dateFormatter.string(from: date)
+    
+    return dateStringLocal
+}
+
+func date_to_datumZeit_string(date: Date) -> String { // "dd.MM.yyyy HH:mm:ss"
+    if date == nil {return ""}
+    
+    let dateFormatter = DateFormatter()
+    dateFormatter.timeZone = TimeZone.current
+    dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+    let dateStringLocal = dateFormatter.string(from: date)
+    
+    return dateStringLocal
+}
+
+func date_to_date_string(date: Date) -> String { // "yyyy-MM-dd"
+    let dateString = date_to_dateTime_string(date: date)
+    
+    return dateString.components(separatedBy: " ")[0]
+}
+
+func date_to_datum_string(date: Date) -> String { // "dd.MM.yyyy"
+    let dateString = date_to_datumZeit_string(date: date)
+    
+    return dateString.components(separatedBy: " ")[0]
+}
+
+func date_to_time_string(date: Date) -> String { // "HH:mm"
+    let dateString = date_to_dateTime_string(date: date)
+    let dateComponents = dateString.components(separatedBy: " ")[1].components(separatedBy: ":")
+    
+    return "\(dateComponents[0]):\(dateComponents[1])"
+}
+
+func date_to_timeSec_string(date: Date) -> String { // "HH:mm:ss"
+    let dateString = date_to_dateTime_string(date: date)
+    
+    return dateString.components(separatedBy: " ")[1]
+}
+
+
+func date_to_dateName(date: Date) -> String { // ["Yesterday", "Today", "Tomorrow"], "yyyy-MM-dd"
+    let dateString = date_to_date_string(date: date)
+    let dateStringToday = date_to_date_string(date: date_getNow())
+    let dateStringTomorrow = date_to_date_string(date: date_offset(days: 1))
+    let dateStringYesterday = date_to_date_string(date: date_offset(days: -1))
+    
+    if dateString == dateStringToday {
+        return "Today"
+    } else if dateString == dateStringTomorrow {
+        return "Tomorrow"
+    } else if dateString == dateStringYesterday {
+        return "Yesterday"
+    }
+    
+    return dateString
+}
+
+func date_to_datumName(date: Date) -> String { // ["Gestern", "Heute", "Morgen"], "yyyy-MM-dd"
+    let dateString = date_to_datum_string(date: date)
+    let dateStringToday = date_to_datum_string(date: date_getNow())
+    let dateStringTomorrow = date_to_datum_string(date: date_offset(days: 1))
+    let dateStringYesterday = date_to_datum_string(date: date_offset(days: -1))
+    
+    if dateString == dateStringToday {
+        return "Heute"
+    } else if dateString == dateStringTomorrow {
+        return "Morgen"
+    } else if dateString == dateStringYesterday {
+        return "Gestern"
+    }
+    
+    return dateString
+}
+
+func date_to_dateName_long(date: Date) -> String { // ["Yesterday", "Today", "Tomorrow"], "yyyy-MM-dd"
+    let dateString = date_to_date_string(date: date)
+    let dateStringToday = date_to_date_string(date: date_getNow())
+    let dateStringTomorrow = date_to_date_string(date: date_offset(days: 1))
+    let dateStringYesterday = date_to_date_string(date: date_offset(days: -1))
+    
+    if dateString == dateStringToday {
+        return "Today \(dateString)"
+    } else if dateString == dateStringTomorrow {
+        return "Tomorrow \(dateString)"
+    } else if dateString == dateStringYesterday {
+        return "Yesterday \(dateString)"
+    }
+    
+    return dateString
+}
+
+func date_to_datumName_long(date: Date) -> String { // ["Gestern", "Heute", "Morgen"], "yyyy-MM-dd"
+    let dateString = date_to_datum_string(date: date)
+    let dateStringToday = date_to_datum_string(date: date_getNow())
+    let dateStringTomorrow = date_to_datum_string(date: date_offset(days: 1))
+    let dateStringYesterday = date_to_datum_string(date: date_offset(days: -1))
+    
+    if dateString == dateStringToday {
+        return "Heute \(dateString)"
+    } else if dateString == dateStringTomorrow {
+        return "Morgen \(dateString)"
+    } else if dateString == dateStringYesterday {
+        return "Gestern \(dateString)"
+    }
+    
+    return dateString
 }
